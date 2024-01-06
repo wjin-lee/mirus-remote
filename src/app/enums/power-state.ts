bf34e7461794f80fca6a0479150221cc6c1ad5c4
Woo Jin Lee <woojinlee337@gmail.com>
2024-01-06 13:57:03 +1300

    Fix power state values to reflect WoL server

--- src/app/enums/power-state.ts
+++ src/app/enums/power-state.ts
@@ -2,23 +2,23 @@
  * Power state of a machine
  */
 export enum PowerState {
-    UNKNOWN,
-    OFFLINE,
-    ONLINE
+  UNKNOWN = -1,
+  OFFLINE = 0,
+  ONLINE = 1,
 }
 
 export function PowerStateFromNumber(state: number): PowerState {
-    switch (state) {
-        case -1:
-            return PowerState.UNKNOWN;
+  switch (state) {
+    case -1:
+      return PowerState.UNKNOWN;
 
-        case 0:
-            return PowerState.OFFLINE;
+    case 0:
+      return PowerState.OFFLINE;
 
-        case 1:
-            return PowerState.ONLINE;
+    case 1:
+      return PowerState.ONLINE;
 
-        default:
-            throw new Error(`Invalid power state value: ${state}!`);
-    }
+    default:
+      throw new Error(`Invalid power state value: ${state}!`);
+  }
 }
--- src/app/wake-on-lan/wake-on-lan.component.ts
+++ src/app/wake-on-lan/wake-on-lan.component.ts
@@ -6,73 +6,79 @@ import { WakeOnLanService } from '../services/wake-on-lan.service';
 @Component({
   selector: 'app-wake-on-lan',
   templateUrl: './wake-on-lan.component.html',
-  styleUrls: ['./wake-on-lan.component.scss']
+  styleUrls: ['./wake-on-lan.component.scss'],
 })
 export class WakeOnLanComponent {
-    // Only need to wake 1 device for now. Eventually make this a query param
-    readonly WOL_TARGET = "orcinus";
+  // Only need to wake 1 device for now. Eventually make this a query param
+  readonly WOL_TARGET = 'orcinus';
 
-    powerState: PowerState = PowerState.UNKNOWN;
-    powerStateMessage: string;
+  powerState: PowerState = PowerState.UNKNOWN;
+  powerStateMessage: string;
 
-    requestQueue: any = [];
-    
-    constructor(private db: Database, private wolService: WakeOnLanService) {
-        // Match default power state message to the default power state.
-        this.powerStateMessage = this._getPowerStateMessage(this.powerState);
+  requestQueue: any = [];
 
-        // Subscribe to power states
-        objectVal(ref(this.db, `powerStates/${this.WOL_TARGET}`)).subscribe(powerState => {
-            console.log(powerState)
-            this.setPowerState(PowerStateFromNumber((powerState) as number));
-        })
+  constructor(private db: Database, private wolService: WakeOnLanService) {
+    // Match default power state message to the default power state.
+    this.powerStateMessage = this._getPowerStateMessage(this.powerState);
 
-        // Subscribe to request queue
-        wolService.requestQueue$.subscribe(requestQueue => {
-            this.requestQueue = requestQueue == null ? [] : Object.values(requestQueue).filter((req: any) => req.device == this.WOL_TARGET);
-        })
+    // Subscribe to power states
+    objectVal(ref(this.db, `powerStates/${this.WOL_TARGET}`)).subscribe(
+      (powerState: any) => {
+        this.setPowerState(PowerStateFromNumber(powerState as number));
+      }
+    );
+
+    // Subscribe to request queue
+    wolService.requestQueue$.subscribe((requestQueue) => {
+      this.requestQueue =
+        requestQueue == null
+          ? []
+          : Object.values(requestQueue).filter(
+              (req: any) => req.device == this.WOL_TARGET
+            );
+    });
+  }
+
+  setPowerState(powerState: PowerState) {
+    this.powerState = powerState;
+    this.powerStateMessage = this._getPowerStateMessage(this.powerState);
+  }
+
+  _getPowerStateMessage(powerState: PowerState) {
+    switch (powerState) {
+      case PowerState.UNKNOWN:
+        return 'Device State Unknown.';
+
+      case PowerState.OFFLINE:
+        return 'Device Offline.';
+
+      case PowerState.ONLINE:
+        return 'Device Online!';
+
+      default:
+        return `Unrecognised power state: ${powerState}.`;
     }
+  }
 
-    setPowerState(powerState: PowerState) {
-        this.powerState = powerState; 
-        this.powerStateMessage = this._getPowerStateMessage(this.powerState);
+  onPowerBtnClick() {
+    switch (this.powerState) {
+      case PowerState.OFFLINE:
+        this.wolService.sendWakeRequest(this.WOL_TARGET);
+        break;
+
+      case PowerState.ONLINE:
+        this.powerStateMessage = 'Device is already online!';
+        break;
+
+      case PowerState.UNKNOWN:
+        break; // Do nothing
+
+      default:
+        this.powerStateMessage = `Unrecognised power state: ${this.powerState}.`;
     }
+  }
 
-    _getPowerStateMessage(powerState: PowerState) {
-        switch(powerState) {
-            case PowerState.UNKNOWN:
-                return "Device State Unknown.";
-
-            case PowerState.OFFLINE:
-                return "Device Offline.";
-
-            case PowerState.ONLINE:
-                return "Device Online!"
-
-            default:
-                return `Unrecognised power state: ${powerState}.`
-        }
-    }
-
-    onPowerBtnClick() {
-        switch(this.powerState) {
-            case PowerState.OFFLINE:
-                this.wolService.sendWakeRequest(this.WOL_TARGET);
-                break;
-
-            case PowerState.ONLINE:
-                this.powerStateMessage = "Device is already online!";
-                break;
-
-            case PowerState.UNKNOWN:
-                break;  // Do nothing
-
-            default:
-                this.powerStateMessage = `Unrecognised power state: ${this.powerState}.`;
-        }
-    }
-
-    onPingBtnClick() {
-        this.wolService.sendDeviceStatusUpdateRequest(this.WOL_TARGET);
-    }
+  onPingBtnClick() {
+    this.wolService.sendDeviceStatusUpdateRequest(this.WOL_TARGET);
+  }
 }
